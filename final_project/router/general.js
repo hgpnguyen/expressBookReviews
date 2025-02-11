d9d44f1e0fb4ffdad37ffbbc91b85f6ed252374b
hgpnguyen <phucgia1808@gmail.com>
2025-02-11 05:03:55 -0500

    Improve task

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,35 +25,47 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.send(JSON.stringify(books, null, 4));
+  new Promise((resolve, reject) => {
+    return res.send(JSON.stringify(books, null, 4));
+  })
+  
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  const book = books[req.params.isbn];
-  if (book) {
-    return res.send(JSON.stringify(book, null, 4));
-} else {
-    return res.status(404).json({ message: 'Book not found.' });
-}
+  new Promise((resolve, reject) => {
+    const book = books[req.params.isbn];
+    if (book) {
+        return res.send(JSON.stringify(book, null, 4));
+    } else {
+        return res.status(404).json({ message: 'Book not found.' });
+    }
+  })
+  
 
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  const author = req.params.author;
-  const booksOfAuthor = Object.values (books).filter((book) => book.author === author);
-  return res.send(JSON.stringify(booksOfAuthor));
+  new Promise((resolve, reject) => {
+    const author = req.params.author;
+    const booksOfAuthor = Object.values (books).filter((book) => book.author === author);
+    return res.send(JSON.stringify(booksOfAuthor));
+  });
+  
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  const title = req.params.title;
-  const booksByTitle = Object.values (books).filter((book) => book.title === title);
-  return res.send(JSON.stringify(booksByTitle));
+  new Promise((resolve, reject) => {
+    const title = req.params.title;
+    const booksByTitle = Object.values (books).filter((book) => book.title === title);
+    return res.send(JSON.stringify(booksByTitle));
+  });
+  
 });
 
 //  Get book review
